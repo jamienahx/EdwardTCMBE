3e6ea7d7d1ceeef57c9c0a84039bfe50c984ee9d
jamienahx <jamie.nahx@gmail.com>
2025-07-15 19:19:33 +0800

    synced BE and FE of pricing page

---
+++ routes/service.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const Service = require("../daos/service");
+
+
+//get all services
+router.get("/",async(req,res) => {
+
+try {
+
+    const services = await Service.find({});
+    res.json(services);
+
+} catch (error) {
+res.status(500).json({message: error.message});
+
+}
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 require("dotenv").config();
 require ("./config/database");
@@ -10,6 +11,7 @@ require ("./config/database");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var appointmentRouter = require('./routes/appointment');
+var serviceRouter = require('./routes/service');
 
 var app = express();
 
@@ -22,10 +24,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/appointment',appointmentRouter);
+app.use('/services', serviceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
